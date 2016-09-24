0be97cf0f2c086e0a50e9be647afaaf5ce89933c
boboy <robertoguazon@gmail.com>
2016-09-24 10:57:12 +0800

    added Activity 4

---
+++ Activity4_GuazonJohnRobert/Assets/act4 assets/scripts/MoveTranslate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTranslate : MonoBehaviour {
+
+    [SerializeField]
+    private float _speed = 2f;
+    [SerializeField]
+    private float _maxDistance = 5f;
+
+    private float _startPosition;
+    private bool _moveRight = true;
+
+	// Use this for initialization
+	void Start () {
+        _startPosition = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = transform.position.x;
+
+        if (x < _maxDistance && _moveRight)
+        {
+            x += _speed * Time.deltaTime;
+        } else
+        {
+            _moveRight = false;
+            x += -_speed * Time.deltaTime;
+           if (x <= _startPosition - _maxDistance)
+            {
+                _moveRight = true;
+            }
+        }
+        transform.position = new Vector3(x,transform.position.y,transform.position.z);
+	}
+}
---
+++ Activity4_GuazonJohnRobert/Assets/quiz1 assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // include UI namespace so can reference UI elements
+
+public class GameManager : MonoBehaviour {
+
+	// static reference to game manager so can be called from other scripts directly (not just through gameobject component)
+	public static GameManager gm;
+
+	// private variables
+	GameObject _player;
+	Vector3 _spawnLocation;
+
+	// set things up here
+	void Awake () {
+		// setup reference to game manager
+		if (gm == null)
+			gm = GetComponent<GameManager>();
+
+		// setup all the variables, the UI, and provide errors if things not setup properly.
+		setupDefaults();
+	}
+
+	// game loop
+	void Update() {
+				Time.timeScale = 1f; // this unpauses the game action (ie. back to normal)
+	}
+
+	// setup all the variables, the UI, and provide errors if things not setup properly.
+	void setupDefaults() {
+		// setup reference to player
+		if (_player == null)
+			_player = GameObject.FindGameObjectWithTag ("Player");
+		
+		if (_player == null)
+			Debug.LogError ("Player not found in Game Manager");
+		
+		// get initial _spawnLocation based on initial position of player
+		_spawnLocation = _player.transform.position;
+	}
+
+}
